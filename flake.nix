2f9a571f8ea5385651932fb82b6a8ea5a06d2222
Cory Duplantis <cld251@gmail.com>
2024-03-18 12:07:52 -0500

    Init basic cargo init with .envrc and direnv allow

---
+++ flake.nix
@@ -0,0 +1,39 @@
+{
+  description = "Basic initialization binary to init a Rust project with envrc and shell.nix";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-21.11";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+
+        rustPackage = pkgs.rustPlatform.buildRustPackage {
+          pname = "cargo_init";
+          version = "0.1.0";
+          src = ./.;
+          cargoSha256 = "0000000000000000000000000000000000000000000000000000"; # Replace this with the actual hash
+          buildInputs = [ ]; # Add dependencies here
+        };
+      in
+      rec {
+        packages.default = rustPackage;
+        defaultPackage = rustPackage;
+
+        apps.default = flake-utils.lib.mkApp { 
+          drv = packages.default; 
+        };
+
+        /*
+        apps.cargo_init = {
+          type = "app";
+          program = "${self.packages.system.cargo_init}/bin/cargo_init}";
+        };
+        */
+      });
+}
---
+++ src/main.rs
@@ -0,0 +1,36 @@
+use std::path::Path;
+
+fn main() {
+    const ENVRC: &'static str = include_str!("../.envrc");
+    const SHELLNIX: &'static str = include_str!("../shell.nix");
+
+    let dir = std::env::args()
+        .nth(1)
+        .expect("USAGE: cargo_init <DIRECTORY>");
+
+    let dir = Path::new(&dir);
+
+    if std::fs::create_dir_all(dir).is_err() {
+        println!("ERROR: Directory already exists: {dir:?}");
+        return;
+    }
+
+    let _ = std::fs::write(dir.join(".envrc"), ENVRC);
+    let _ = std::fs::write(dir.join("shell.nix"), SHELLNIX);
+
+    std::process::Command::new("direnv")
+        .current_dir(dir)
+        .args(["allow"])
+        .spawn()
+        .expect(&format!("Failed to direnv allow in {dir:?}"));
+
+    std::process::Command::new("nix-shell")
+        .args([
+            "-p",
+            "cargo",
+            "--run",
+            "cargo init ./macroquad_breakout_juicing",
+        ])
+        .spawn()
+        .expect(&format!("Failed to cargo init in {dir:?}"));
+}
