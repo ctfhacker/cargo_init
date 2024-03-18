528c1e77078ca128ef35da6731f9caec3e60cb54
Cory Duplantis <cld251@gmail.com>
2024-03-18 12:34:48 -0500

    Add .gitignore

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
+use std::io::Write;
 use std::path::Path;
 
 fn main() {
     const ENVRC: &'static str = include_str!("../.envrc");
     const SHELLNIX: &'static str = include_str!("../shell.nix");
+    const GITIGNORE: &'static str = include_str!("../.gitignore");
 
     let dir = std::env::args()
         .nth(1)
@@ -15,15 +17,14 @@ fn main() {
         return;
     }
 
-    let _ = std::fs::write(dir.join(".envrc"), ENVRC);
-    let _ = std::fs::write(dir.join("shell.nix"), SHELLNIX);
-
+    // `direnv allow` in the new directory
     std::process::Command::new("direnv")
         .current_dir(dir)
         .args(["allow"])
         .spawn()
         .expect("Failed to direnv allow");
 
+    // Execute `cargo init` in the new directory
     std::process::Command::new("nix-shell")
         .args([
             "-p",
@@ -33,4 +34,9 @@ fn main() {
         ])
         .spawn()
         .expect("Failed to cargo init");
+
+    // Write the wanted files into the new project
+    let _ = std::fs::write(dir.join(".envrc"), ENVRC);
+    let _ = std::fs::write(dir.join("shell.nix"), SHELLNIX);
+    let _ = std::fs::write(dir.join(".gitignore"), GITIGNORE);
 }
