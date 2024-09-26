b5224d0fff33e484feb2d0d2e99ddb73b88765c2
Cory Duplantis <cld251@gmail.com>
2024-09-25 20:12:35 -0500

    Updates

---
+++ README.md
@@ -0,0 +1,5 @@
+# Project templates
+
+```
+nix flake new --template github:ctfhacker/nix_templates#rust ./new_rust_project
+```
--- files/flake.nix
+++ files/flake.nix
@@ -1,10 +1,10 @@
 {
-  description = "Snapchange development environment";
+  description = "CHANGEME";
 
   # Flake inputs
   inputs = {
     # Nixpkgs
-    nixpkgs.url = "github:nixos/nixpkgs/release-23.11";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
     # Rust overlay for Rust nightly
     rust-overlay.url = "github:oxalica/rust-overlay";
@@ -25,44 +25,16 @@
         };
 
         myrust = pkgs.rust-bin.nightly.latest.default.override {
-          extensions = [ "rust-src "]
-          targets = []
+          extensions = [ "rust-src" ];
+          targets = [];
         };
       in
         with pkgs; {
           devShells.default = mkShell {
-              # LD_LIBRARY_PATH = lib.makeLibraryPath [ pkgs.stdenv.cc.cc ];
-
               packages = [
-                # Python
-                (python312.withPackages (ps: with ps; [
-                  virtualenv 
-                  pip 
-                  ipython
-                  ipdb
-                ]))
-
-                # Rust nightly
                 myrust
                 rust-analyzer
               ];
-
-              shellHook = ''
-                # Install binaryninja into a local virtual envirionment
-                # If the virtual environment has not been created, create it
-                if [ ! -d "./.venv" ]; then
-                  BINJA=$(readlink $(which binaryninja))
-                  BINJA_PATH=$(dirname $BINJA)/..
-
-                  python3 -m venv ./.venv;
-                  source ./.venv/bin/activate;
-
-                  python3 $BINJA_PATH/opt/scripts/install_api.py --install-on-pyenv
-                else
-                  # If the virtual environment exists, use it
-                  source ./.venv/bin/activate
-                fi
-              '';
             };
           });
 }
